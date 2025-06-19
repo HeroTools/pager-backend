aa50d2e8acdf8dfc335bf60d014782e01113223e
Zach Bresler <zbresler16@gmail.com>
2025-06-19 22:41:48 +0200

    feat: get invite token

---
+++ functions/auth/get-invite-link/helpers/get-member.ts
@@ -0,0 +1,14 @@
+import { supabase } from '../utils/supabase-client';
+
+const getMember = async (workspaceId: string, userId: string) => {
+    const { data: member } = await supabase
+        .from('workspace_members')
+        .select('id')
+        .eq('workspace_id', workspaceId)
+        .eq('user_id', userId)
+        .single();
+
+    return member;
+};
+
+export { getMember };
