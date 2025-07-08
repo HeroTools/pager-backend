e3b5f4d6abeb2499d7fdbbd4854581d6d092db6a
Zach Bresler <zbresler16@gmail.com>
2025-07-08 14:22:25 +0200

    refactor: reactions

--- functions/reactions/user-reactions/app.ts
+++ functions/reactions/user-reactions/app.ts
@@ -1,8 +1,8 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-import { getUserIdFromToken } from './helpers/auth';
-import { supabase } from './utils/supabase-client';
-import { successResponse, errorResponse } from './utils/response';
-import { getMember } from './helpers/get-member';
+import { getUserIdFromToken } from '../../common/helpers/auth';
+import { supabase } from '../../common/utils/supabase-client';
+import { successResponse, errorResponse } from '../../common/utils/response';
+import { getMember } from '../../common/helpers/get-member';
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
